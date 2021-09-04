04d16f93ed6647a280d381c1f66231d2671d85d3
sahananeelappa <sahana.neelappa@xyramsoft.com>
2021-09-04 22:44:40 +0530

    create edit get all project response change

--- src/main/java/com/xyram/ticketingTool/service/ProjectService.java
+++ src/main/java/com/xyram/ticketingTool/service/ProjectService.java
@@ -3,20 +3,21 @@ package com.xyram.ticketingTool.service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import com.xyram.ticketingTool.apiresponses.ApiResponse;
 import com.xyram.ticketingTool.entity.Employee;
 import com.xyram.ticketingTool.entity.Projects;
 
 public interface ProjectService {
 
-	Projects addproject(Projects project); 
+	ApiResponse addproject(Projects project); 
 		
 	
 
-	Page<Projects> getAllProjects(Pageable pageable);
+	ApiResponse getAllProjects(Pageable pageable );
 
 
 
-	Projects editEmployee(Integer projectId, Projects projectRequest); 
+	ApiResponse editEmployee(Integer projectId, Projects projectRequest); 
 		
 
 }
