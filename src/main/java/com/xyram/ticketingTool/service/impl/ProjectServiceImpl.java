32afc9a1c8e89a0e98e0fc7d355a928da20ce3e3
SubrahmanyamAllu <87303124+SubrahmanyamAllu@users.noreply.github.com>
2021-09-06 21:21:43 +0530

    Update ProjectServiceImpl.java

--- src/main/java/com/xyram/ticketingTool/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/xyram/ticketingTool/service/impl/ProjectServiceImpl.java
@@ -15,8 +15,8 @@ import org.springframework.stereotype.Service;
 
 import com.xyram.ticketingTool.Repository.ProjectRepository;
 import com.xyram.ticketingTool.apiresponses.ApiResponse;
-import com.xyram.ticketingTool.entity.Employee;
 import com.xyram.ticketingTool.entity.Projects;
+import com.xyram.ticketingTool.request.CurrentUser;
 import com.xyram.ticketingTool.service.ProjectService;
 import com.xyram.ticketingTool.util.ResponseMessages;
 
@@ -33,70 +33,73 @@ public class ProjectServiceImpl implements ProjectService {
 
 	@Autowired
 	ProjectRepository projectRepository;
+	
+	@Autowired
+	CurrentUser userDetail;
 
 	@Override
 	public ApiResponse addproject(Projects project) {
-		ApiResponse response = validateProjects(project);
-		if (response.isSuccess()) { //
-			projectRepository.save(project);
+		ApiResponse response = validateClientId(project);
+		if (response.isSuccess()) { 
+			//projectRepository.save(project);
 			project.setCreatedAt(new Date());
-
+			project.setCreatedBy(userDetail.getUserId());
+			project.setLastUpdatedAt(new Date());
+			project.setUpdatedBy(userDetail.getUserId());
+			//System.out.println("project.getCreatedBy - " + project.getCreatedBy());
+			//System.out.println("userDetail.getUserId() - " + userDetail.getUserId());
 			Projects projetAdded = projectRepository.save(project);
+			
 			response.setSuccess(true);
 			response.setMessage(ResponseMessages.PROJECT_ADDED);
-			Map content = new HashMap();
+			Map<String, String> content = new HashMap<String, String>();
 			content.put("projectId", projetAdded.getpId());
 			response.setContent(content);
 			return response;
-
 		}
 		return response;
 	}
 
-	private ApiResponse validateProjects(Projects projects) {
+	private ApiResponse validateClientId(Projects projects) {
 		ApiResponse response = new ApiResponse(false);
 		if (projects.getClientId()!= null) {
 			response.setMessage("success");
 			response.setSuccess(true);
 			response.setContent(null);
 		} else {
-
 			response.setMessage(ResponseMessages.ClIENT_ID_VALID);
-
 			response.setSuccess(false);
+			response.setContent(null);
 		}
 		return response;
 	}
 
 	@Override
 	public ApiResponse getAllProjects(Pageable pageable) {
-
 		Page<Map> projectList =   projectRepository.getAllProjectLsit(pageable);
 		Map content = new HashMap();
-	       content.put("projectList", projectList);
-	       ApiResponse response = new ApiResponse(true);
-	       response.setSuccess(true);
-	       response.setContent(content);
-	       return  response;
-		}
+	    content.put("projectList", projectList);
+	    ApiResponse response = new ApiResponse(true);
+	    response.setSuccess(true);
+	    response.setContent(content);
+	    return  response;
+	}
 	
 	
 	@Override
 	public ApiResponse editEmployee(String projectId, Projects projectRequest) {
 
-		ApiResponse response = validateProjects(projectRequest);
+		ApiResponse response = validateClientId(projectRequest);
 		if (response.isSuccess()) {
 			  Projects projects = projectRepository.getById(projectId);
 			if (projects != null) { 
-			
-		
 				projects.setUpdatedBy(projectRequest.getUpdatedBy());
 				projects.setLastUpdatedAt(new Date());
 				projects.setInHouse(projectRequest.getInHouse());
 				projects.setProjectDescritpion(projectRequest.getProjectDescritpion());
 				projects.setProjectName(projectRequest.getProjectName());
 				projects.setStatus(projectRequest.getStatus()); 
-				projectRepository.save(projectRequest);
+				//projectRepository.save(projectRequest);			
 				Projects projetAdded = projectRepository.save(projectRequest);
 
 				response.setSuccess(true);
@@ -104,19 +107,13 @@ public class ProjectServiceImpl implements ProjectService {
 				Map content = new HashMap();
 				content.put("projectId", projetAdded.getpId());
 				response.setContent(content);
-
 			}
-
 			else {
 				response.setSuccess(false);
 				response.setMessage(ResponseMessages.PROJECT_ID_VALID);
 				response.setContent(null);
 			}
-
 		}
-
 		return response;
 	}
-
-	
 }
