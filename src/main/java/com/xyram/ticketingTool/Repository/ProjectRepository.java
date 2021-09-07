0e01f33186c1ac7e1952813a547f99f9fd81b316
SubrahmanyamAllu <87303124+SubrahmanyamAllu@users.noreply.github.com>
2021-09-07 12:47:41 +0530

    Client name in Proj List

--- src/main/java/com/xyram/ticketingTool/Repository/ProjectRepository.java
+++ src/main/java/com/xyram/ticketingTool/Repository/ProjectRepository.java
@@ -1,5 +1,6 @@
 package com.xyram.ticketingTool.Repository;
 
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.data.domain.Page;
@@ -14,4 +15,8 @@ public interface  ProjectRepository extends  JpaRepository<Projects,String> {
 	
 	@Query("Select new map(e.pId as id,e.projectName as projectName,e.projectDescritpion as projectDescritpion,e.clientId as clientId,e.inHouse as inHouse,e.status as status) from Projects e")
 	Page<Map> getAllProjectLsit(Pageable pageable);
+	
+	@Query(value = "Select p.project_id as pId, p.project_name as projectName, p.project_description as projectDescritpion,\r\n"
+			+ "p.client_id as clientId, c.client_name, p.inhouse as inHouse, p.projectstatus as status from project p join client c ON p.client_id = c.client_id", nativeQuery = true)
+	List<Map> getAllProjectsList();
 }
