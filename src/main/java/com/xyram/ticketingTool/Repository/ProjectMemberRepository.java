3f8e5cb699392e895d48391f3f761a0851328439
sahananeelappa <sahana.neelappa@xyramsoft.com>
2021-09-14 12:16:13 +0530

    get  all project by employee id

--- src/main/java/com/xyram/ticketingTool/Repository/ProjectMemberRepository.java
+++ src/main/java/com/xyram/ticketingTool/Repository/ProjectMemberRepository.java
@@ -5,13 +5,22 @@ import java.util.Map;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.xyram.ticketingTool.entity.ProjectMembers;
 
+@Repository
 public interface ProjectMemberRepository extends JpaRepository<ProjectMembers, String> {
-	@Query("Select new map(e.employeeId as employeeId) from ProjectMembers e")
-
-	List<Map> getAllProjectByEmployeeId(String employeeId);
+	
+	@Query("Select new map(e.projectId as projectId,p.projectName as projectName) from ProjectMembers e left join  Projects p On e.projectId = p.pId where e.employeeId=:employeeId" )
+    List<Map> getAllProjectByEmployeeId(String employeeId);
 //	public List<ProjectMembers> findByProject_pIdAndEmployee_eId(Integer pId,Integer eId);
 
 }
+/*
+ * //
+ * ("Select new map(e.eId as id, e.firstName as firstName, e.lastName as lastName, case when p.projectId = :projectId then 1 else 0 end as projectAssignedStatus) "
+ * "
+ * "from Employee e left JOIN ProjectMembers p On e.eId = p.employeeId where e.status = 'ACTIVE'"
+ * )
+ */
