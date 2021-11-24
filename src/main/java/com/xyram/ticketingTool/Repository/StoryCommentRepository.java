6f4187a7626c194345cbbeefc6cb0fff2ce00697
bharath M L <bharathnaidu2601@gmail.com>
2021-11-24 11:48:37 +0530

    story comment

--- src/main/java/com/xyram/ticketingTool/Repository/StoryCommentRepository.java
+++ src/main/java/com/xyram/ticketingTool/Repository/StoryCommentRepository.java
@@ -1,6 +1,10 @@
 package com.xyram.ticketingTool.Repository;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.xyram.ticketingTool.entity.StoryComments;
@@ -8,4 +12,22 @@ import com.xyram.ticketingTool.entity.StoryComments;
 @Repository
 public interface StoryCommentRepository extends JpaRepository<StoryComments, String>{
 
+	@Query("SELECT new map(sc.description as description,sc.mentionTo as mentionTo,sc.commentedDate as commentedDate"
+			+ " ,sc.storyId as storyId,p1.projectName as projectName,"
+			+ "CONCAT(e1.firstName ,' ', e1.lastName) as coommentedBy,sc.storyAttachment as storyAttachment)  from StoryComments sc"
+			+ " left join Projects p1 on p1.pId=sc.projectId "
+			+   "left join sc.storyAttachment sa "
+			+ " left join Employee e1 on e1.eId=sc.commentedBy WHERE sc.projectId=:id"  )
+	List<Map> getCommentsByProjectId(String id);
+
+	@Query("SELECT new map(sc.description as description,sc.mentionTo as mentionTo,sc.commentedDate as commentedDate"
+			+ " ,sc.storyId as storyId,p1.projectName as projectName,"
+			+ "CONCAT(e1.firstName ,' ', e1.lastName) as coommentedBy,sc.storyAttachment as storyAttachment)  from StoryComments sc"
+			+ " left join Projects p1 on p1.pId=sc.projectId "
+			+   "left join sc.storyAttachment sa "
+			+ " left join Employee e1 on e1.eId=sc.commentedBy WHERE sc.projectId=:projectId AND sc.storyId=:storyId"  )
+	List<Map> getStoryCommentbyprojectIdandStoryId(String projectId,String storyId);
+
+	// sc.mentionTo as mentionTo,sc.commentedDate as commentedDate,sc.storyId as storyId, "
+//	+ " p1.projectName,CONCAT(e1.firstName ,' ', e1.lastName) as coommentedBy,sc.storyAttachment as storyAttachment)
 }
--- src/main/java/com/xyram/ticketingTool/service/StoryCommentService.java
+++ src/main/java/com/xyram/ticketingTool/service/StoryCommentService.java
@@ -1,10 +1,17 @@
 package com.xyram.ticketingTool.service;
 
+import java.util.List;
+import java.util.Map;
+
 import com.xyram.ticketingTool.entity.StoryComments;
 import com.xyram.ticketingTool.vo.StoryCommentVo;
 
 public interface StoryCommentService {
 
 	StoryComments CreateStoryComment(StoryCommentVo storyComment);
+
+	List<Map> getStoryCommentbyprojectId(String Id);
+
+	List<Map> getStoryCommentbyprojectIdandStoryId(String Id,String sId);
 
 }
