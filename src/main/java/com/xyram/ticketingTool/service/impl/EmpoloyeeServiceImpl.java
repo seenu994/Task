d3c917438f6ea5969da76b2f49c1491081d990ab
chandinisundar <chandini.s@xyramsoft.com>
2021-12-08 22:52:43 -0800

    changes

--- src/main/java/com/xyram/ticketingTool/service/impl/EmpoloyeeServiceImpl.java
+++ src/main/java/com/xyram/ticketingTool/service/impl/EmpoloyeeServiceImpl.java
@@ -666,12 +666,10 @@ public class EmpoloyeeServiceImpl implements EmployeeService {
 	public ApiResponse getEmployeeDetails(String employeeId) {
 		ApiResponse response = new ApiResponse(false);
 		Map employee = employeeRepository.getbyEmpId(employeeId);
-		Map content = new HashMap();
-		content.put("employeeDetails", employee);
 		if (employee != null) {
 			response.setSuccess(true);
 			response.setMessage("Employee Retrieved Successfully");
-			response.setContent(content);
+			response.setContent(employee);
 		}
 
 		else {
