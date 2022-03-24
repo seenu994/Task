1e2ab47f24b27bc94633dcfbcde4db39a86e4ab2
RaviKumar-19 <ravi.kumar@xyramsoft.com>
2022-03-24 14:33:40 +0530

    asset table

---
+++ src/main/java/com/xyram/ticketingTool/service/impl/SoftwaresInstalledServiceImpl.java
@@ -0,0 +1,13 @@
+package com.xyram.ticketingTool.service.impl;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
+import com.xyram.ticketingTool.service.SoftwaresInstalledService;
+
+@Service
+@Transactional
+public class SoftwaresInstalledServiceImpl implements SoftwaresInstalledService {
+
+}
