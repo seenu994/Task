9d6ec522e0da87512e84b5a206b0c394947fa985
RaviKumar-19 <ravi.kumar@xyramsoft.com>
2022-04-19 10:19:57 +0530

    asset changes

--- src/main/java/com/xyram/ticketingTool/entity/Asset.java
+++ src/main/java/com/xyram/ticketingTool/entity/Asset.java
@@ -50,13 +50,13 @@ public class Asset extends AuditModel{
 	@Column(name = "ram")
 	private String ram;
 	
-	@Column(name = "bag_available")
+	@Column(name = "bag_available", nullable = true)
 	private boolean bagAvailable;
 
-	@Column(name = "powercord_available")
+	@Column(name = "powercord_available", nullable = true)
 	private boolean powercordAvailable;
 
-	@Column(name = "mouse_available")
+	@Column(name = "mouse_available", nullable = true)
 	private boolean mouseAvailable;
 
 	@Column(name = "asset_photo_url", nullable = true)
@@ -64,7 +64,7 @@ public class Asset extends AuditModel{
 	
 	@Enumerated(EnumType.STRING)
 	@Column(name = "asset_status")
-	private AssetStatus assetStatus = AssetStatus.AVAILABLE;
+	private AssetStatus assetStatus;
 	
 //	@Column(name = "assigned_to")
 //	private String assignedTo;
