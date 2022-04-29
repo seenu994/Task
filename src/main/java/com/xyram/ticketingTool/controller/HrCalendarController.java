f083ce4fb49b87bc4375fc0f7f344374847d30e3
hema sundar <hema.sundar@xyramsoft.com>
2022-04-29 12:15:23 +0530

    added

--- src/main/java/com/xyram/ticketingTool/controller/HrCalendarController.java
+++ src/main/java/com/xyram/ticketingTool/controller/HrCalendarController.java
@@ -155,8 +155,8 @@ public class HrCalendarController {
 		return hrCalrendarService.searchSchedulesInCalender(searchString);
 	} 
 	
-	@GetMapping(value = { AuthConstants.HR_ADMIN_BASEPATH + "/searchSchedulesInCalender/{searchString}",
-			AuthConstants.HR_BASEPATH + "/searchSchedulesInCalender/{searchString}"			
+	@GetMapping(value = { AuthConstants.HR_ADMIN_BASEPATH + "/searchSchedulesInCalender/{mobileNo}",
+			AuthConstants.HR_BASEPATH + "/searchSchedulesInCalender/{mobileNo}"			
 			})
 	ApiResponse getCandidateHistory(@PathVariable String mobileNo) {
 		logger.info("Get all getCandidateHistory");
