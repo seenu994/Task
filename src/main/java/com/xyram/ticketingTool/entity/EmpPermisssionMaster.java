d0659d137171639bb044d252dec8a413d5b6b13a
hema sundar <hema.sundar@xyramsoft.com>
2022-05-30 15:17:50 +0530

    RemoinderRequest class added

---
+++ src/main/java/com/xyram/ticketingTool/entity/EmpPermisssionMaster.java
@@ -0,0 +1,59 @@
+package com.xyram.ticketingTool.entity;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import com.xyram.ticketingTool.baseData.model.AuditModel;
+import com.xyram.ticketingTool.id.generator.IdGenerator;
+import com.xyram.ticketingTool.id.generator.IdPrefix;
+
+
+@Entity
+@Table(name = "emp_permission_master")
+public class EmpPermisssionMaster {
+	
+	@Id
+	@IdPrefix(value = "PER_MAS")
+	@GeneratedValue(generator = IdGenerator.ID_GENERATOR)
+	@GenericGenerator(name = IdGenerator.ID_GENERATOR, strategy = "com.xyram.ticketingTool.id.generator.IdGenerator")
+	@Column(name = "id")
+	private String Id;
+	
+	@Column(name = "name", nullable = false)
+	private String name;
+	
+	@Column(name = "description", nullable = false)
+	private String description;
+
+	public String getId() {
+		return Id;
+	}
+
+	public void setId(String id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	
+}
